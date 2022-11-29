9b1198705f41ef3506d0c740c36219bcae558b89
iotiotdotin <iotiotdotin@gmail.com>
2022-11-29 03:21:43 +0000

    added alcohol and AQI sensors

--- alcohol-sensor.py
+++ alcohol-sensor.py
@@ -1,13 +1,30 @@
-import smbus
+# Simple demo of reading each analog input from the ADS1x15 and printing it to
+# the screen.
+# Author: Tony DiCola
+# License: Public Domain
 import time
 
-I2C_PORT = 7
-PCF8591_ADDR = 0x48
-bus = smbus.SMBus(I2C_PORT)
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
 
+# Note you can change the I2C address from its default (0x48), and/or the I2C
+# bus by passing in these optional parameters:
+adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=7)
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 16
+
+print('Reading Alcohol values, press Ctrl-C to quit...')
 while True:
-    bus.write_byte(PCF8591_ADDR, A0)
-    value = bus.read_byte(PCF8591_ADDR)
-    print("=======================")
-    print("Alcohol range: %f PPM" % value)
-    time.sleep(1)
+    # Read all the ADC channel values in a list.
+    value = adc.read_adc(1, gain=GAIN)
+    print("Alcohol: %.2f PPM" % value)
+    time.sleep(0.5)
